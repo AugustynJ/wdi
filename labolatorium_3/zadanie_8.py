cea1b72ee6207d7300b14bf43c9290d71631749c
AugustynJ <augustynj@student.agh.edu.pl>
2021-11-02 10:15:32 +0100

    zadansy
    zadansy

--- labolatorium_3/zadanie_8.py
+++ labolatorium_3/zadanie_8.py
@@ -1,15 +1,31 @@
 import sys
+import math
 n = int(input(""))
 
+strona = -1
+odl = -1
 for i in range (0, n):
     spacje = n-i-1
     for j in range (0, spacje):
         sys.stdout.write(str(" "))
+
     if i == 0:
         sys.stdout.write(str("X"))
+        print ("")
+        continue
+
     for j in range (spacje, n+i):
-        if j == spacje+1 or j == n+i-2:
-            sys.stdout.write(str("O"))
+        if j == n+1 - odl*strona:
+            sys.stdout.write(str("o"))
         else:
             sys.stdout.write(str("*"))
-    print ("")
+    
+    if i%2 == 1:
+         odl = odl + 2
+    strona = strona * (-1)
+    print("")
+
+
+for i in range (0, n -1):
+    sys.stdout.write(str(" "))
+print ("U")
---
+++ labolatorium_3/zadanie_9.py
@@ -0,0 +1,35 @@
+praca = 1
+saldo = 1000
+PIN = 2137
+while (praca > 0) :
+    print("Wybierz operacje:")
+    print("I - wplata")
+    print("O - wyplata")
+    print("S - sprawdz saldo")
+    print("E - zakoncz")
+    op = (input(""))
+    if op != "E":
+        print("Podaj PIN:")
+        pin = int(input(""))
+        while(pin != PIN):
+            print("PIN nieprawidlowy")
+            print("Wprowadz PIN ponownie:")
+            pin = int(input(""))
+    
+    if op == "I":
+        print("Podaj kwotę wpłaty")
+        kwota = int(input(""))
+        saldo += kwota
+    elif op == "O":
+        print("Podaj kwotę wypłaty")
+        kwota = int(input(""))
+        saldo -= kwota
+    elif op == "S":
+        print("Twoje saldo wynosi: %d" %saldo)
+    elif op == "E":
+        praca = 0
+    if op != "E":
+        print("Co chcesz zrobic w kolejnym kroku?")
+
+print("Zapraszamy ponownie")
+    
