9442f21e4f34b26ec98793818e3d8eda5bc179d6
AugustynJ <augustynj@student.agh.edu.pl>
2021-12-13 10:01:11 +0100

    initial commit

--- labolatorium_10/sudoku.py
+++ labolatorium_10/sudoku.py
@@ -1,5 +1,12 @@
 import math
 
+def printuj(T):
+    for i in range(9):
+        for j in range(9):
+            print(T[i][j], end = ' ')
+        print("")
+    return None
+
 def spr (w, k, T, P):
     for i in range(9):
         if(P[w][i] != 0):
@@ -11,8 +18,9 @@ def spr (w, k, T, P):
     k3 = math.floor(k/3)
     for i in range(9):
         for j in range(9):
-            if(math.floor(i) == w3) and (math.floor(j) == k3): 
-                if(P[w][i] != 0):
+            if(math.floor(i/3) == w3) and (math.floor(j/3) == k3): 
+                #if(w==6 and k==3): print(i, j, P[i][j])
+                if(P[i][j] != 0):
                     T[w][k][P[i][j]-1] = 0
     return None
 
@@ -26,7 +34,7 @@ P = [
     [8, 0, 0, 6, 7, 0, 0, 3, 0],
     [0, 5, 0, 0, 0, 0, 9, 0, 0],
     [3, 1, 9, 0, 0, 0, 2, 7, 8],
-    [0, 0, 2, 0, 0, 0, 0, 4, 0],
+    [0, 0, 2, 9, 0, 0, 0, 4, 0],
     [0, 7, 0, 0, 6, 9, 0, 0, 0],
     [5, 0, 4, 2, 8, 0, 0, 0, 0],
     [0, 0, 6, 0, 1, 0, 8, 0, 0],
@@ -41,6 +49,7 @@ while(zera > 0):
         for j in range(9):
             if(P[i][j] == 0):
                 spr(i, j, T, P)
+    # print(zera)
     for i in range(9):
         for j in range(9):
             if(P[i][j] != 0): continue
@@ -49,8 +58,11 @@ while(zera > 0):
             if(s == 1):
                 for k in range(9):
                     if(T[i][j][k] == 1):
-                        print("JD")
+                        #print("JD")
                         P[i][j] = k+1
                         zera -= 1
                         break
-    
+    #for i in range(9): print(T[6][3][i], end = ' ')
+    if(zera == 44): 
+        printuj(P)
+        exit(0)
--- labolatorium_10/zadanie_10.py
+++ labolatorium_10/zadanie_10.py
@@ -70,5 +70,37 @@ for _ in range(0, 5):
             elif(x == 90): print("cos pi/2 = 0", file = zapis)
             elif(x == 180): print("cos pi = -1", file = zapis)
             elif(x == 360): print("cos 2*pi = 1", file = zapis)
-
-    
+    elif(f == "tan"):
+        if(not rad):
+            if(x == 0): print("tan 0 = 0", file = zapis)
+            elif(x == 30): print("tan 30 = sqrt(3)/3 = 0.577", file = zapis)
+            elif(x == 45): print("tan 45 = 1", file = zapis)
+            elif(x == 60): print("tan 60 = sqrt(3) = 1.732", file = zapis)
+            elif(x == 90): print("tan 90 nie istnieje", file = zapis)
+            elif(x == 180): print("tan 180 = 0", file = zapis)
+            elif(x == 360): print("tan 360 = 0", file = zapis)
+        else:
+            if(x == 0): print("tan 0 = 0", file = zapis)
+            elif(x == 30): print("tan pi/6 = sqrt(3)/3 = 0.577", file = zapis)
+            elif(x == 45): print("tan pi/4 = 1", file = zapis)
+            elif(x == 60): print("tan pi/3 = sqrt(3) = 1.732", file = zapis)
+            elif(x == 90): print("tan pi/2 nie istnieje", file = zapis)
+            elif(x == 180): print("tan pi = 0", file = zapis)
+            elif(x == 360): print("tan 2*pi = 0", file = zapis)
+    elif(f == "ctg"):
+        if(not rad):
+            if(x == 0): print("ctg 0 nie istnieje", file = zapis)
+            elif(x == 30): print("ctg 30 = sqrt(3) = 1.732", file = zapis)
+            elif(x == 45): print("ctg 45 = 1", file = zapis)
+            elif(x == 60): print("ctg 60 = sqrt(3)/3 = 0.577", file = zapis)
+            elif(x == 90): print("ctg 90 = 0", file = zapis)
+            elif(x == 180): print("ctg 180 nie istnieje", file = zapis)
+            elif(x == 360): print("ctg 360 nie istnieje", file = zapis)
+        else:
+            if(x == 0): print("ctg 0 nie istnieje", file = zapis)
+            elif(x == 30): print("ctg pi/6 = sqrt(3) = 1.732", file = zapis)
+            elif(x == 45): print("ctg pi/4 = 1", file = zapis)
+            elif(x == 60): print("ctg pi/3 = sqrt(3)/3 = 0.577", file = zapis)
+            elif(x == 90): print("ctg pi/2 = 0", file = zapis)
+            elif(x == 180): print("ctg pi = 0", file = zapis)
+            elif(x == 360): print("ctg 2*pi = 0", file = zapis)
